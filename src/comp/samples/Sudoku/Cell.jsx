db1dc3f50e48bfb1c587c4e838cf560eea5f3d21
Enrique Mendez <enrique.phys@gmail.com>
2023-09-28 19:04:37 -0700

    Add handleClick, and handleKeypress events.

--- src/comp/samples/Sudoku/Cell.jsx
+++ src/comp/samples/Sudoku/Cell.jsx
@@ -1,43 +1,30 @@
-import React, {useRef, useEffect, useState} from 'react'
-import styled from 'styled-components'
+//cell.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { squareItemToRowColumn } from './SudokuApp';
 
-// Breakpoints and colors (keeping as you had them)
-const mobile = "375px";
-const mini = "433px";
-const desktop = "1440px";	
-const purple = "hsl(259deg, 100%, 65%)";
-const lightRed = "hsl(0deg, 100%, 67%)";
-const white = "hsl(0, 0%, 100%)";
-const offWhite = "hsl(0, 0%, 94%)";
-const lightGrey = "hsl(0, 0%, 86%)";
-const smokeyGrey = "hsl(0, 1%, 44%)";
-const offBlack = "hsl(0, 0%, 8%)";
+const CellContainer = styled.div`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  outline: 1px solid #BEC6D4;
+  outline-offset: -1px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 35px;
+`
 
+const Cell = ({ value, square, item, setSelected }) => {
+  return (
+    <CellContainer onClick={() => setSelected([square, item])}>
+      {value !== 0 ? value : ''}
+    </CellContainer>
+  );
+};
 
-export default function Cell({value, notes, parentRef}) {
-	const fraction = 10/100;
-	const [containerHeight, setContainerHeight] = useState(0);
-	
-	useEffect(()=>{
-	      	const containerElement = parentRef.current;
-	      	const height = containerElement.clientWidth;
-	      	setContainerHeight(height * fraction);
-	},[]);	
+Cell.propTypes = {
+  value: PropTypes.number
+};
 
-	// Styling for the NoteCell
-	const NoteCell = styled.div`
-		font-size: ${containerHeight*fraction}px;
-		display: flex;
-		border: 1px solid black;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		aspect-ratio: 1 / 1;
-	`;
-
-	return (
-		<NoteCell>
-				{value ? value : notes}
-		</NoteCell>
-	);
-}
+export default Cell;
--- src/comp/samples/Sudoku/SudokuApp.jsx
+++ src/comp/samples/Sudoku/SudokuApp.jsx
@@ -1,33 +1,98 @@
-import React, {useRef, useEffect} from 'react'
-import styled from 'styled-components'
-import Cell from './Cell'
-//colors
-const mobile = "375px";
-const mini = "433px"
-const desktop = "1440px";	
-//colors
-const purple = "hsl(259deg, 100%, 65%)"
-const lightRed = "hsl(0deg, 100%, 67%)"
-const white = "hsl(0, 0%, 100%)"
-const offWhite = "hsl(0, 0%, 94%)"
-const lightGrey = "hsl(0, 0%, 86%)"
-const smokeyGrey = "hsl(0, 1%, 44%)"
-const offBlack = "hsl(0, 0%, 8%)"
+//sudokuapp.jsx
+import React, { useRef, useState, useEffect} from 'react';
+import styled from 'styled-components';
+import Cell from './Cell';
+import MiniGrid, {MiniGridContainer, InnerBorder} from './MiniGrid';  // Make sure to import MiniGrid
+
+const floor = Math.floor;
+
+const Card = styled.div`
+	font-family: 'Arial Rounded MT Bold';
+	padding: 5rem;
+	width: 100%;
+	aspect-ratio: 413 / 653;
+	background: #F5F6F8;
+	box-shadow: 0px 10px 30px 10px rgba(0, 0, 0, 0.25);
+	border-radius: 10px;
+`;
 
 const Board = styled.div`
-	font-family: 'Noto Sans Mono';
-	width: 100%;
+	font-weight: 100;
+	padding: 0;
 	aspect-ratio: 1;
-`
+	// display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	outline: 1px solid black;
+`;
 
-export default function SudokuApp(props){
-	const parentRef = useRef(null)
-	let list = new Array(81).fill(0)
-
-	
-	return (
-		<Board ref={parentRef}>
-			<Cell value={2} parentRef={parentRef}/>
-		</Board>
-	)
+export function squareItemToRowColumn(square, item){
+	const row   	= 3*floor(square / 3) + floor(item / 3)
+	const column	= 3*floor(square % 3) + floor(item % 3)
+	return [row, column]
 }
+
+export function rowColumnToSquareItem(row, column){
+	const square   	= floor(row / 3) + floor(column / 3)
+	const subrow   	= row % 3
+	const subcolumn	= column % 3
+	const item     	= 3 * subrow + subcolumn
+	return [square, item]
+}
+
+export default function SudokuApp(props) {
+	//state vars
+	let [board, setBoard] = useState([
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null,null],
+		])
+	let [selectedCell, setSelectedCell] = useState([null, null])
+
+	const handleKeyPress = (event) => {
+		const { key } = event;
+
+		if (key >= '1' && key <= '9') {
+			let [square, item] = selectedCell;
+			if (square != null && item != null) {
+				let newBoard = [...board]
+				newBoard[square][item] = Number(key)
+				setBoard(newBoard) 
+			}
+		}
+	}
+
+	useEffect(()=>{
+		window.addEventListener('keypress', handleKeyPress);
+		return () => {
+			window.removeEventListener('keypress', handleKeyPress)
+		}
+	}, [selectedCell])
+
+	return (
+		<Card>
+			<h1>Sudoku</h1>
+			<Board>
+				<div style={{outline: '1px solid black'}}>
+				<MiniGridContainer>
+					{board.map((miniGridValues, index) => (
+						<MiniGrid 
+							key={index} 
+							square={index} 
+							values={miniGridValues} 
+							setSelected={setSelectedCell}/>
+					))}
+				</MiniGridContainer>
+				</div>
+			</Board>
+		</Card>
+	);
+}
+
