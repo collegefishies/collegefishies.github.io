7fd30bf7d2312f2a57664dbd65ffc53bb4d935ae
Enrique Mendez <enrique.phys@gmail.com>
2023-10-02 08:22:51 -0700

    Add hash router.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React            	from 'react';
-import ReactDOM         	from 'react-dom/client';
-import App              	from './App';
-import reportWebVitals  	from './reportWebVitals';
-import { BrowserRouter }	from 'react-router-dom';
+import React                        	from 'react';
+import ReactDOM                     	from 'react-dom/client';
+import App                          	from './App';
+import reportWebVitals              	from './reportWebVitals';
+import { BrowserRouter, HashRouter }	from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>  
+		</HashRouter>  
 	</React.StrictMode>
 );
 
