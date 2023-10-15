3843df00a3e0b1dee4a4b88a15b439392687b622
Enrique Mendez <enrique.phys@gmail.com>
2023-10-15 16:06:15 -0700

    Add profile pic.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
+import styled from 'styled-components'
 import ContentArea	from '../comp/ContentArea'
 import { Card }	from '../comp/BlogPost'
 import Sidebar    	from '../comp/Sidebar'
 import UsefulLinks	from '../comp/UsefulLinks'
 import Content    	from '../comp/Content'
 import Row from 'react-bootstrap/Row'
+import profile 	from '../assets/imgs/profile.jpeg'
+
+const Profile = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+const ProfilePicture = styled.img`
+	width: 300px;
+	border-radius: 50%;
+`
+
 
 function Main() {
 	return ( 
@@ -12,6 +25,9 @@ function Main() {
 					{/*<Sidebar />*/}
 					<Content title="About me">
 						<Card style={{padding: '1rem'}}>
+							<Profile>
+								<ProfilePicture src={profile} />
+							</Profile>
 							My name is Enrique Mendez. I have a PhD in Physics from MIT where I studied Quantum Physics in Ultracold Atom Systems as well as independently formulated a classical formulation of Quantum Mechanics. When my thesis is live, I'll post it here. This website is for following and documenting my more programming interests.
 							Here are my other interests in no particular order:
 							<div class='m-3'>
